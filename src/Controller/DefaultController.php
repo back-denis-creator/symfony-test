5a6f978fcb8b1ec37cea0157e0a8b84c39ab6d99
Denis B <denis.b@zina.com.ua>
2022-02-05 07:53:38 +0200

    Update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,12 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="homepage")
      */
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('main/default/index.html.twig', []);
     }
 }
